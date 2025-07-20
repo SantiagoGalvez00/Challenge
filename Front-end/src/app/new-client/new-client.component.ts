88331900dbaf7d60ad3618d968ee341b112e61a0
SantiagoGalvez00 <galvez.santiago.92@gmail>
2025-07-20 15:15:54 -0300

    new client ok

--- Front-end/src/app/new-client/new-client.component.ts
+++ Front-end/src/app/new-client/new-client.component.ts
@@ -1,31 +1,59 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import dayjs from 'dayjs';
 import axios from 'axios';
 
 @Component({
   selector: 'app-new-client',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './new-client.component.html',
   styleUrl: './new-client.component.css'
 })
 export class NewClientComponent {
 
   newClient: FormGroup;
+  toDay: string = dayjs().format('YYYY-MM-DD');
+  showMessage: boolean = false;
+  notValid: boolean = false;
+  message: string = ""
 
   constructor(
     private formBuilder: FormBuilder
   ) {
     this.newClient = this.formBuilder.group({
-      nombre: '',
-      apellido: '',
-      edad: 0,
-      fechaNacimiento: dayjs().format('YYYY-MM-DD')
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      edad: [18, Validators.required],
+      fechaNacimiento: [dayjs().format('YYYY-MM-DD'), Validators.required]
     })
   }
 
-  async onSubmit() {
-    await axios.post('http://localhost:5000/crear-cliente', this.newClient.value)
+  async onSubmit(event: Event) {
+    event.preventDefault();
+
+    this.showMessage = false;
+    if (this.newClient.valid){
+      await axios.post('http://localhost:5000/crear-cliente', this.newClient.value)
+      this.message = "Cliente Guardado correctamente"
+      this.showMessage = true;
+      this.notValid = false;
+      this.newClient.reset({
+        nombre: '',
+        apellido: '',
+        edad: 18,
+        fechaNacimiento: dayjs().format('YYYY-MM-DD')
+      })
+    } else {
+      this.message = "!Faltan datos requeridos. Revise el formulario antes de continuar¡"
+      this.showMessage = true;
+      this.notValid = true;
+    }
+
+    setInterval(
+      () => {this.showMessage = false},
+      10000
+    )
   }
 
 }
